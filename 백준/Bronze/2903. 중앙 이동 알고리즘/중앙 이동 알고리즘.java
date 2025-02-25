ab2490a8c9ecff3fe315f6e5a051d7927ad4018e
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2025-02-25 17:18:19 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
+        Scanner sc =  new Scanner(System.in);
         
-        int n = s.nextInt();
+        int n = sc.nextInt();
         
-        int a = (int)Math.pow(2,n)+1;
-        
-        int result = a*a;
-        
-        System.out.println(result);
+        int a = 0;
+        for(int i = 0; i<n; i++){
+            a = (1 << n) +1;
+
+            }
+            int result = a*a;
+            System.out.println(result);
+        }
     }
-}
