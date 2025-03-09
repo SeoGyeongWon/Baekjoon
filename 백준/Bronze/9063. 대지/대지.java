ca308603c03714c21da0efca07c211aed0439fa8
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2025-03-09 17:14:02 +0900

    [Bronze III] Title: 대지, Time: 728 ms, Memory: 102304 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -2,32 +2,24 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         
         int n = sc.nextInt();
         
         int maxX = Integer.MIN_VALUE, minX = Integer.MAX_VALUE;
-        
         int maxY = Integer.MIN_VALUE, minY = Integer.MAX_VALUE;
         
-       
-        
-        for(int i =0; i <n; i++){
-            int x = sc.nextInt();
-            int y=  sc.nextInt();
+        for(int i = 0; i<n; i++){
+            int x= sc.nextInt();
+            int y= sc.nextInt();
             
-            if(x>maxX){maxX = x;}
-            if(x<minX){minX = x;}
-            if(y>maxY){maxY = y;}
-            if(y<minY){minY = y;}
+            maxX = Math.max(maxX,x);
+            minX = Math.min(minX,x);
+            maxY = Math.max(maxY, y);
+            minY = Math.min(minY, y);
         }
         
-        int row = maxX-minX;
-        
-        int col = maxY-minY;
-        
-        System.out.println(row*col);
-       
+        System.out.println((maxX-minX)*(maxY-minY));
         sc.close();
     }
 }
