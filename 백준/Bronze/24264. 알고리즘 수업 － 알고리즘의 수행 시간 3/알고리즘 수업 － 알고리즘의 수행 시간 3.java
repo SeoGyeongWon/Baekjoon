fe6054856502bedbe15a5edb652cf28b64e06005
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2025-03-25 20:22:03 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 180 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+         Long n = new Scanner(System.in).nextLong();
+        
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
