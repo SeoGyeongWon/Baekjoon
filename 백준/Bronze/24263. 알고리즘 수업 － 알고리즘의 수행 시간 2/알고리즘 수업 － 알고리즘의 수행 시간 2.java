7f42d7d22c5d3e88399c06f843eb63b5b6c5e112
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2025-03-26 16:44:16 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -1,8 +1,11 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-int n = new Scanner(System.in).nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        
+        long n = Long.parseLong(r.readLine());
+
         System.out.println(n);
         System.out.println(1);
     }
