ab01aafb002cd2c8171d6bafd35fa396f2faded6
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2025-03-27 20:47:46 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,28 @@
 import java.io.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
+
+        int n =Integer.parseInt(br.readLine());
         
         int count = 0;
         
         while(n>=0){
-            if(n%5 == 0){
+            if(n%5==0){
                 count += n/5;
-                bw.write(String.valueOf(count));
-                bw.flush();
-                bw.close();
-                return;
+                break;
             }
-            n -=3;
-            count++;
+            else{
+                n -=3;
+                count++;
+                }
         }
-        bw.write(String.valueOf(-1));
+        if(n<0){
+            count = -1;
+        }
+        bw.write(String.valueOf(count));
         bw.flush();
         bw.close();
     }
