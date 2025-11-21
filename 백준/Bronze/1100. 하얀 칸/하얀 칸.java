9b78854136c55d1add6e7e1ace1fa813ab0b7560
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2025-11-22 04:02:27 +0900

    [Bronze II] Title: 하얀 칸, Time: 168 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.java
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -3,42 +3,30 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+               
         
-        char[][] arr = new char[8][8];
+        boolean[][] isWhite = new boolean[8][8];
         
         for(int i = 0; i<8; i++){
-            if(i % 2== 0){
-                for(int j = 0; j<8; j++){
-                    if(j%2 == 0){
-                        arr[i][j] = 'W';
-                    }
-                    else if(j % 2== 1){
-                        arr[i][j] = 'B';
-                    }
+            for(int j = 0; j<8; j++){
+                if(i%2 ==1 && j%2==1){
+                    isWhite[i][j] = true;
+                }
+                else if(i%2==0 && j%2 ==0){
+                    isWhite[i][j] = true;                   
                 }
             }
-            else if(i%2 ==1){
-                for(int j = 0; j < 8; j++){
-                    if(j%2 == 1){
-                        arr[i][j] = 'W';
-                    }
-                    else if(j%2 == 0){
-                        arr[i][j] = 'B';
-                    }
-                }
-            }
-            
         }
         int count = 0;
         for(int i = 0; i<8; i++){
-           String s = sc.next();
-            for(int j= 0; j<8; j++){
+            String s = sc.nextLine();
+            for(int j = 0; j<8; j++){
                 char c = s.charAt(j);
-                if(arr[i][j] == 'W' && c == 'F'){
+                if(c == 'F' && isWhite[i][j] == true){
                     count++;
                 }
             }
         }
-        System.out.print(count);
+            System.out.println(count);       
     }
 }
